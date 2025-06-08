4821cc8f8e6efa7988735758d36181039df2c3e4
kiyoon <46194046+parkgiyun@users.noreply.github.com>
2025-06-08 20:38:58 +0900

    검증 메시지 문구 통일

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/entity/Product.java
@@ -29,7 +29,7 @@ public class Product {
     @NotEmpty(message = "제조국은 비어있을 수 없습니다")
     private String madeIn;
     
-    @NotNull(message = "가격은 null일 수 없습니다")
+    @NotNull(message = "가격은 비어있을 수 없습니다")
     @Min(value = 0, message = "가격은 0 이상이어야 합니다")
     private Double price;
 
